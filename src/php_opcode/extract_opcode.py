0031945ea68aa46234b20c61f3c53139d2479c74
goodpeanuts <goddpeanuts@foxmail.com>
2025-05-07 19:09:38 +0800

    extract opcode

--- src/php_opcode/extract_opcode.py
+++ src/php_opcode/extract_opcode.py
@@ -1,8 +1,6 @@
 from gettext import find
-import pprint
 import subprocess
 import os
-import filter
 import re
 import csv
 from typing import List
@@ -10,6 +8,8 @@ import glob
 from datetime import datetime
 import logging
 from tqdm import tqdm
+import collect
+import shutil
 
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 os.makedirs("./logs", exist_ok=True)
@@ -73,17 +73,47 @@ def split_by_opcode_tables(content: str) -> List[List[str]]:
 
 
 """
-从文件中提取操作码表并保存为 CSV 文件
+从内容中提取操作码表并返回 CSV 格式的字符串
 """
-def extract_from_file(filepath: str, output_dir: str = "./csv_output"):
+def extract_opcode(content: str) -> str:
+    tables = split_by_opcode_tables(content)
+    output = []
+
+    for idx, table_lines in enumerate(tables):
+        rows = parse_opcode_table(table_lines)
+        csv_output = []
+        writer = csv.DictWriter(csv_output, fieldnames=[
+            "line", "op_index", "flag", "op", "fetch", "ext", "ret", "operands"
+        ])
+        writer.writeheader()
+        writer.writerows(rows)
+        output.append("\n".join(csv_output))
+
+    return "\n\n".join(output)
+
+"""
+从文件中提取操作码表并提取opcode
+"""
+def extract_from_file(filepath: str):
     with open(filepath, "r", encoding="utf-8", errors="ignore") as f:
         content = f.read()
 
-    tables = split_by_opcode_tables(content)
-    filename_prefix = os.path.basename(filepath).split('.')[0]
+    csv_content = extract_opcode(content)
+
+    return csv_content
+
+"""
+从文件中提取操作码表并保存为 CSV 文件
+"""
+def extract_opcode_from_file_and_save(filepath: str, output_dir: str = "./csv_output"):
+    csv_content = extract_from_file(filepath)
+
+    # Generate filename prefix by replacing '.' and '/' with '_'
+    filename_prefix = re.sub(r'[./]', '_', os.path.relpath(filepath, start=os.getcwd())).lstrip('_')
 
     os.makedirs(output_dir, exist_ok=True)
 
+    tables = split_by_opcode_tables(csv_content)
     for idx, table_lines in enumerate(tables):
         rows = parse_opcode_table(table_lines)
         outpath = os.path.join(output_dir, f"{filename_prefix}_{idx}.csv")
@@ -97,7 +127,21 @@ def extract_from_file(filepath: str, output_dir: str = "./csv_output"):
 
         print(f"[+] Wrote {outpath} ({len(rows)} rows)")
 
+    print(f"[+] Completed writing CSV files for {filepath}")
+
 def start_docker_container() -> str | None:
+    # 检查是否有已存在的容器
+    existing_container_id = subprocess.check_output(
+        [
+            'docker', 'ps', '-q', '--filter', 'name=php_vld_container'
+        ],
+        text=True
+    ).strip()
+
+    if len(existing_container_id) != 0:
+        logging.info(f"[+] Existing Docker container found: {existing_container_id}")
+        return existing_container_id
+    
     """启动一个长期运行的 Docker 容器"""
     try:
         container_id = subprocess.check_output(
@@ -125,11 +169,14 @@ def stop_docker_container(container_id: str):
         logging.error(f"[!] Error stopping Docker container: {e}")
 
 
-def extract_opcodes(container_id: str, file_path: str):
+def container_extract_opcodes(container_id: str, file_path: str):
     """在已有的 Docker 容器中提取 PHP 文件的操作码"""
     try:
         abs_path = os.path.abspath(file_path)
         rel_path = os.path.relpath(abs_path, os.getcwd())
+
+        # 确保文件在挂载目录内
+        assert abs_path.startswith(os.getcwd()), f"File {file_path} is outside the mounted directory."
 
         docker_cmd = [
             'docker', 'exec', container_id,
@@ -163,46 +210,57 @@ def print_files():
         assert os.path.exists(file), f"File not found: {file}"
         print(file)
 
-# 示例用法
+
+"""
+提取 PHP 扩展的操作码表, 分别将stdout和stderr输出到文件
+"""
 if __name__ == "__main__":
-    # print_files()
-    files = find_stderr_txt_files("./filter/php")
-    for f in files :
-        extract_from_file(f, output_dir="./csv_output")
+    container_id = start_docker_container()
+    if not container_id:
+        logging.error("[-] Failed to start Docker container.")
+        exit(1)
 
-    # container_id = start_docker_container()
-    # if not container_id:
-    #     logging.error("[-] Failed to start Docker container.")
-    #     exit(1)
 
-    # # src_directory = "./repo"
-    # # dest_directory = "./filter"
-    # src_directory = "./manual"
-    # dest_directory = "./filter/manual"
-    # allowed_file_extensions = [".php"]
+    w, b, _, _ = collect.collect_files()
 
-    # phps = filter.filter_and_clean_files(src_directory, dest_directory, allowed_file_extensions)
+    if os.path.exists("repo"):
+        shutil.rmtree("repo")
+    os.makedirs("repo/0", exist_ok=True)
+    os.makedirs("repo/1", exist_ok=True)
+    new_w = []
+    new_b = []
 
-    # try:
-    #     # 多次提取操作码
-    #     for php_file in tqdm(phps, desc="Extracting opcodes"):
-    #         (stdout, stderr) = extract_opcodes(container_id, php_file)
-    #         if stdout:
-    #             out_file = os.path.splitext(php_file)[0] + "_opcode.stdout.txt"
-    #             with open(out_file, "w") as f:
-    #                 f.write(stdout)
-    #             logging.info(f"[+] Opcode written to {out_file}")
-                
-    #         else:
-    #             logging.error(f"[-] Failed to extract opcode stdout for {php_file}.")
+    for file_path in w:
+        filename_prefix = re.sub(r'[./]', '_', os.path.relpath(file_path, start=os.getcwd())).lstrip('_')
+        dest_path = os.path.join("repo/0", filename_prefix)
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        shutil.copy(file_path, dest_path)
+        new_w.append(dest_path)
 
-    #         if stderr:
-    #             err_file = os.path.splitext(php_file)[0] + "_opcode.stderr.txt"
-    #             with open(err_file, "w") as f:
-    #                 f.write(stderr)
-    #             logging.info(f"[+] Opcode error written to {err_file}")
-    #         else:
-    #             logging.error(f"[-] Failed to extract opcode stderr for {php_file}.")
-    # finally:
-    #     # 确保程序结束时停止 Docker 容器
-    #     stop_docker_container(container_id)
+    for file_path in b:
+        filename_prefix = re.sub(r'[./]', '_', os.path.relpath(file_path, start=os.getcwd())).lstrip('_')
+        dest_path = os.path.join("repo/1", filename_prefix)
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        shutil.copy(file_path, dest_path)
+        new_b.append(dest_path)
+
+    try:
+        # 多次提取操作码
+        for php_file in tqdm(new_w + new_b, desc="Extracting white opcodes"):
+            (stdout, stderr) = container_extract_opcodes(container_id, php_file)
+            if stdout:
+                out_file = os.path.splitext(php_file)[0] + "_opcode.stdout.txt"
+                with open(out_file, "w") as f:
+                    f.write(stdout)                
+            else:
+                logging.error(f"[-] Failed to extract opcode stdout for {php_file}.")
+
+            if stderr:
+                err_file = os.path.splitext(php_file)[0] + "_opcode.stderr.txt"
+                with open(err_file, "w") as f:
+                    f.write(stderr)
+            else:
+                logging.error(f"[-] Failed to extract opcode stderr for {php_file}.")
+    finally:
+        # 确保程序结束时停止 Docker 容器
+        stop_docker_container(container_id)
